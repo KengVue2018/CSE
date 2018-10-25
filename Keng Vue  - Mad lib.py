d352e83c63f678d662a9ecbd051327f8543fb8b8
KengVue2018 <42385517+KengVue2018@users.noreply.github.com>
2018-10-25 09:45:03 -0700

    Keng vue
    Mad lib

---
+++ Keng Vue  - Mad lib.py
@@ -0,0 +1,7 @@
+print("Last summer, my mom and dad took me and on a trip to %s." % (noun, places))
+print("The weather there is very ______!")
+print("Northern _________ has many ________ and they make ________, _________ there.")
+print("Many people also go to _________ or see the _______.")
+print("The people that live there love to eat ________ and are very proud of their big ______.")
+print("They also like to _______ in the sun and swim in the ________! It was a really __________ trip!")
+
