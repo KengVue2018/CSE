bbc6446a46ae53241759c226eefb58bcbca87a85
KengVue2018 <42385517+KengVue2018@users.noreply.github.com>
2018-10-30 09:46:03 -0700

    helloworlds added loopes

--- Keng Vue  - Mad lib.py
+++ Keng Vue  - Mad lib.py
@@ -1,12 +1,12 @@
-name = input
-places = input
-nouns = input
-verbs = input
-abjective = input
-pronouns = input
-country = input
-animals = input
-Food = input
+name = input("Type in an name: ")
+places = input(" Type in an places: ")
+nouns = input("Type in an nouns: ")
+verbs = input("Type in an verbs: ")
+abjective = input("Type in an abjective: ")
+pronouns = input("Tyoe in an pronouns: ")
+country = input("Type in an country: ")
+animals = input("Type in an animals: ")
+Food = input("Tyoe in an Food: ")
 print("Last summer, my mom and dad took me and %s on a trip to %s." % (name, places))
 print("The weather there is very %s!" % abjective)
 print("Northern %s has many %s and they make %s, %s there." % (country, animals, abjective, Food))
