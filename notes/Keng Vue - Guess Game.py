36d5a73623886a86f96d52e1bbb2c2602cefa328
KengVue2018 <42385517+KengVue2018@users.noreply.github.com>
2018-11-08 09:46:25 -0800

    guess game keng

--- notes/Keng Vue - Guess Game.py
+++ notes/Keng Vue - Guess Game.py
@@ -1,13 +1,15 @@
 import random
-random.randint(0, 10)
+r = random.randint(0, 10)
 guesses_left = 5
 playing = True
+
+print("Type a number 1-10")
+
 while guesses_left > 0 and playing:
-    print("You have %d guesses left" % guesses_left)
-    current_guess = int(input("Type in a number between 1 and 10"))
-    if current_guess == number:
-        print("YOU WIN!!!!!! HUZZAH!!!!")
-        playing = False
+    
+
+
+
 
 
 
