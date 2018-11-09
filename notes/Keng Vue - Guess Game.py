3b3156f4fb0410ff3af3713db46e05b70fdd6bcf
KengVue2018 <42385517+KengVue2018@users.noreply.github.com>
2018-11-09 10:31:43 -0800

    keng's guess game
    finished guess game

--- notes/Keng Vue - Guess Game.py
+++ notes/Keng Vue - Guess Game.py
@@ -6,7 +6,19 @@ playing = True
 print("Type a number 1-10")
 
 while guesses_left > 0 and playing:
-    
+    guess = int(input("Guess="))
+    if guess > r:
+        print("Lower")
+        guesses_left -= 1
+    elif guess < r:
+        print("Greater")
+        guesses_left -= 1
+    else:
+        print("Corrent!")
+        playing = False
+
+
+
 
 
 
