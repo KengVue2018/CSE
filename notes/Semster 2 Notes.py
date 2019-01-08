cdb042edf0de9f8119ed5013812b9ca0141d607f
KengVue2018 <42385517+KengVue2018@users.noreply.github.com>
2019-01-08 09:45:00 -0800

    NOtes for semster 2

---
+++ notes/Semster 2 Notes.py
@@ -0,0 +1,28 @@
+print("HEllO WORLD")
+
+#This is a single line comment
+
+
+cars = 5
+driving = True
+
+print("I have %d cars" % cars)
+print("I have " + str(cars) + " cars")
+
+age = input("How old are you?")
+print(age + "??? you really shouldn't lie about your age")
+
+colors = ["red", "blue", "orange", "purple", "pink"]
+print(colors)
+
+colors.append("yellow")
+print(colors)
+
+colors.pop(0)
+print(colors)
+
+print(colors[2])
+
+print(len(colors))
+
+
