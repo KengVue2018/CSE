3415aac88d0c44e9b818b56af52597131dc56353
KengVue2018 <42385517+KengVue2018@users.noreply.github.com>
2019-01-11 10:39:14 -0800

    hio

--- notes/KENG VUE - HANGMAN.py
+++ notes/KENG VUE - HANGMAN.py
@@ -1,8 +1,14 @@
-list = ["HI", "DO", "MY", "HAVE", "ANYTHING", "GOOD", "BAD", "FOOD", "BREAK", "HELL"]
-
 import random
+word_list = ["HI", "DO", "MY", "HAVE", "ANYTHING", "GOOD", "BAD", "FOOD", "BREAK", "HELL"]
+
 guesses_left = 8
 
-there_letters = int()
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+
+
+
 
 
