3dec26dd70e13daf0804c56a431a04ec2e2124f8
KengVue2018 <42385517+KengVue2018@users.noreply.github.com>
2019-01-25 09:44:58 -0800

    hi 4

--- notes/KENG VUE - HANGMAN.py
+++ notes/KENG VUE - HANGMAN.py
@@ -5,25 +5,30 @@ list_of_letters = string.ascii_letters
 
 turns = 8
 
-output = []
+letters_guessed = []
 word_selection = random.choice(word_list)
 length = len(word_selection)
 for i in range(length):
-    output.append("_ ")
-print("".join(output))
+    letters_guessed.append("_ ")
+print("".join(letters_guessed))
 print(word_selection)
 
 playing = True
 
 while turns > 0 and playing:
-    user_guess = input("Guess a letter:")
-    print('\n * 10')
-    if user_guess.lower() in word_selection or user_guess.upper() in word_selection:
-        print("You got it right")
-        for i in range(len(word_selection)):
-            if word_selection[i].lower() == user_guess.lower():
-                output.pop(i)
-                
+    hidden_word = []
+    for letter in word_selection:
+        if letter in letters_guessed:
+            hidden_word.append(letter)
 
+        else:
+            hidden_word.append("")
+            print("".join(hidden_word))
 
-
+            user_guess = input("Guess a letter:")
+            print('\n * 10')
+            if user_guess.lower() in word_selection or user_guess.upper() in word_selection:
+                print("You got it right")
+                for i in range(len(word_selection)):
+                    if word_selection[i].lower() == user_guess.lower():
+                        letters_guessed.pop(i)
