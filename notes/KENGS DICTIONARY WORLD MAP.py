c881782d5054532b0a1908ecdfd90bac668f25c1
KengVue2018 <42385517+KengVue2018@users.noreply.github.com>
2019-01-31 09:44:33 -0800

    hi9

--- notes/KENGS DICTIONARY WORLD MAP.py
+++ notes/KENGS DICTIONARY WORLD MAP.py
@@ -6,6 +6,13 @@ world_map = {
         'PATHS': {
             'NORTH': "PARKING_LOT",
         }
+    },
+    'PARKING_LOT': {
+        'NAME': "The North Parking Lot",
+        'DESCRIPTION': "There are a couple cars parked here.",
+        'PATHS': {
+            'SOUTH': 'R19A'
+        }
     }
 
 },
@@ -13,12 +20,19 @@ world_map = {
 # Controller
 playing = True
 current_node = world_map['R19A']
-while True:
+directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'UP', 'DOWN']
+
+while playing:
     print(current_node['NAME'])
     print(current_node['DESCRIPTION'])
     command = input(">_")
-    if command.lower()in ['q', 'quit','exit']:
+    if command.lower() in ['q', 'quit', 'exit']:
         playing = False
-
-
-
+    elif command.upper() in directions:
+        try:
+            room_name = current_node['PATHS'][command.upper()]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way")
+    else:
+        print("Command Not Found")
