91c2b0505e50e8a11621ed2b19a16398c6012e78
KengVue2018 <42385517+KengVue2018@users.noreply.github.com>
2019-03-01 10:39:07 -0800

    hiu

---
+++ notes/Items.py
@@ -0,0 +1,10 @@
+class Weapon(object):
+    def __init__(self, sword, name):
+        super(Weapon, self).__init__(name)
+        self.duration = 100
+        self.sword = sword
+        self.damage = 10
+    
+    def __
+
+
