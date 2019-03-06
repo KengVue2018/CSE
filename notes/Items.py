67c5b2e5ecb84a89d53abf518af74478ea48f4f3
KengVue2018 <42385517+KengVue2018@users.noreply.github.com>
2019-03-06 09:44:09 -0800

    Idems 5

--- notes/Items.py
+++ notes/Items.py
@@ -5,34 +5,41 @@ class Item(object):
 
 class Weapon(Item):
     def __init__(self, sword):
+        super(Weapon, self).__init__(sword)
         self.duration = 100
         self.sword = sword
         self.damage = 10
 
-
-class shield(Item):
-    def __init__(self, name, shield):
-        super(shield, self).__init__(name)
+    def __init__(self, shield):
+        super(shield, self).__init__(shield)
         self.duration = 100
         self.shield = shield
         self.blocks_damage = 10
 
 
 class Armor(Item):
-    def __init__(self, Helmet):
-        super(Armor, self).__init__(Helmet)
+    def __init__(self, helmet):
+        super(Armor, self).__init__(helmet)
         self.duration = 100
         self.blocks_damage = 10
 
-    def __init__(self, Chest_Armor):
-        super(Armor, self).__init__(Chest_Armor)
+    def __init__(self, chest_armor):
+        super(Armor, self).__init__(chest_armor)
         self.duration = 100
         self.blocks_damage = 25
 
-    def __init__(self, Leg_Armor):
-        super(Armor, self).__init__(Leg_Armor)
+    def __init__(self, leg_armor):
+        super(Armor, self).__init__(leg_armor)
         self.duration = 100
         self.blocks_damage = 25
+
+    def __init__(self, boots):
+        super(Armor, self).__init__(boots)
+        self.duration = 100
+        self.blocks_damage = 10
+
+class Consumables(Item):
+    def __init__(self):
 
 
 
