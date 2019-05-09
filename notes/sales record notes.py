a180519cbb96efe54116aa376d62a7de268a97af
KengVue2018 <42385517+KengVue2018@users.noreply.github.com>
2019-05-09 09:46:38 -0700

    sales notes keng j

--- notes/sales record notes.py
+++ notes/sales record notes.py
@@ -5,7 +5,8 @@ with open("Sales Records.csv") as old_csv:
     reader = csv.reader(old_csv)
 
     for row in reader:
-        old_number = row[2]
-        print(old_number)
+        old_number = row[13]
+        if row[2] == 'Fruits':
+            print(old_number)
 
 print("DONE")
