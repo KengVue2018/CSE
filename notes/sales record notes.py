e70a7dd306f49605622112259691f42b056159aa
KengVue2018 <42385517+KengVue2018@users.noreply.github.com>
2019-05-13 09:46:19 -0700

    sales records fifnish

--- notes/sales record notes.py
+++ notes/sales record notes.py
@@ -33,6 +33,7 @@ with open("Sales Records.csv") as old_csv:
 
 
 total = round(total, 2)
+print("------")
 print("The total clothes profit was:")
 print("${:,}".format(total))
 
@@ -51,6 +52,7 @@ with open("Sales Records.csv") as old_csv:
 
 
 total = round(total, 2)
+print("------")
 print("The total MeaT profit was:")
 print("${:,}".format(total))
 
@@ -69,6 +71,7 @@ with open("Sales Records.csv") as old_csv:
 
 
 total = round(total, 2)
+print("------")
 print("The total Beverages profit was:")
 print("${:,}".format(total))
 
@@ -87,8 +90,140 @@ with open("Sales Records.csv") as old_csv:
 
 
 total = round(total, 2)
+print("------")
 print("The total Office Supplies profit was:")
 print("${:,}".format(total))
 
 
+with open("Sales Records.csv") as old_csv:
+    reader = csv.reader(old_csv)
+    total = 0
+    for row in reader:
+        if row[0] == 'Region':
+            continue
+            # old_number = int(row[0]) + 1
+        old_number = row[13]
+        if row[2] == 'Cosmetics':
+            total += float(old_number)
+            # print(old_number)
+
+
+total = round(total, 2)
+print("------")
+print("The total Cosmetics profit was:")
+print("${:,}".format(total))
+
+
+with open("Sales Records.csv") as old_csv:
+    reader = csv.reader(old_csv)
+    total = 0
+    for row in reader:
+        if row[0] == 'Region':
+            continue
+            # old_number = int(row[0]) + 1
+        old_number = row[13]
+        if row[2] == 'Snacks':
+            total += float(old_number)
+            # print(old_number)
+
+
+total = round(total, 2)
+print("------")
+print("The total Snacks profit was:")
+print("${:,}".format(total))
+
+
+
+with open("Sales Records.csv") as old_csv:
+    reader = csv.reader(old_csv)
+    total = 0
+    for row in reader:
+        if row[0] == 'Region':
+            continue
+            # old_number = int(row[0]) + 1
+        old_number = row[13]
+        if row[2] == 'Personal Care':
+            total += float(old_number)
+            # print(old_number)
+
+
+total = round(total, 2)
+print("------")
+print("The total Personal Care profit was:")
+print("${:,}".format(total))
+
+
+with open("Sales Records.csv") as old_csv:
+    reader = csv.reader(old_csv)
+    total = 0
+    for row in reader:
+        if row[0] == 'Region':
+            continue
+            # old_number = int(row[0]) + 1
+        old_number = row[13]
+        if row[2] == 'Household':
+            total += float(old_number)
+            # print(old_number)
+
+
+total = round(total, 2)
+print("------")
+print("The total Household profit was:")
+print("${:,}".format(total))
+
+
+with open("Sales Records.csv") as old_csv:
+    reader = csv.reader(old_csv)
+    total = 0
+    for row in reader:
+        if row[0] == 'Region':
+            continue
+            # old_number = int(row[0]) + 1
+        old_number = row[13]
+        if row[2] == 'Vegetables':
+            total += float(old_number)
+            # print(old_number)
+
+
+total = round(total, 2)
+print("------")
+print("The total Vegetables profit was:")
+print("${:,}".format(total))
+
+with open("Sales Records.csv") as old_csv:
+    reader = csv.reader(old_csv)
+    total = 0
+    for row in reader:
+        if row[0] == 'Region':
+            continue
+            # old_number = int(row[0]) + 1
+        old_number = row[13]
+        if row[2] == 'Baby Food':
+            total += float(old_number)
+            # print(old_number)
+
+
+total = round(total, 2)
+print("------")
+print("The total Baby Food profit was:")
+print("${:,}".format(total))
+
+
+with open("Sales Records.csv") as old_csv:
+    reader = csv.reader(old_csv)
+    total = 0
+    for row in reader:
+        if row[0] == 'Region':
+            continue
+            # old_number = int(row[0]) + 1
+        old_number = row[13]
+        if row[2] == 'Cereal':
+            total += float(old_number)
+            # print(old_number)
+
+
+total = round(total, 2)
+print("------")
+print("The total Cereal profit was:")
+print("${:,}".format(total))
 
